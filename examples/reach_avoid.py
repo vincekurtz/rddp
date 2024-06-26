b3da6c893edd31ddff503b1dfa08accd3d7a1d89
Vince Kurtz <vjkurtz@gmail.com>
2024-06-26 13:38:12 -0700

    rough cut at fitting a score model

--- examples/reach_avoid.py
+++ examples/reach_avoid.py
@@ -1,7 +1,11 @@
+import pickle
+
 import jax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
+import optax
 
+from rddp.architectures import DiffusionPolicyMLP
 from rddp.data_generation import (
     AnnealedLangevinOptions,
     DatasetGenerationOptions,
@@ -53,7 +57,7 @@ def solve_with_gradient_descent() -> None:
     plt.show()
 
 
-def plot_dataset() -> None:
+def generate_dataset(save: bool = False) -> None:
     """Generate some data and make plots of it."""
     rng = jax.random.PRNGKey(0)
     x0 = jnp.array([-0.1, -1.5])
@@ -76,6 +80,12 @@ def plot_dataset() -> None:
     # Generate some data
     rng, gen_rng = jax.random.split(rng)
     dataset = generator.generate(gen_rng)
+
+    # Save the data if requested
+    fname = "/tmp/reach_avoid_dataset.pkl"
+    if save:
+        generator.save_dataset(dataset, fname)
+        print(f"Saved dataset to {fname}")
 
     # Make some plots
     gamma = langevin_options.noise_decay_rate
@@ -116,6 +126,86 @@ def plot_dataset() -> None:
     plt.show()
 
 
+def fit_score_model() -> None:
+    """Fit a simple scor32e model to the generated data."""
+    rng = jax.random.PRNGKey(0)
+
+    # Load training data from a file (must run generate_dataset first)
+    fname = "/tmp/reach_avoid_dataset.pkl"
+    with open(fname, "rb") as f:
+        data = pickle.load(f)
+    dataset = data["dataset"]
+    dataset = dataset.replace(k=dataset.k.reshape(-1, 1))  # TODO: fix earlier
+
+    # Split the data into training and validation sets
+    rng, split_rng = jax.random.split(rng)
+    idxs = jax.random.permutation(split_rng, len(dataset.x0))
+    train_idxs = idxs[: int(0.8 * len(idxs))]
+    val_idxs = idxs[int(0.8 * len(idxs)) :]
+
+    train_dataset = jax.tree.map(lambda x: x[train_idxs], dataset)
+    val_dataset = jax.tree.map(lambda x: x[val_idxs], dataset)
+
+    # Initialize the score model
+    net = DiffusionPolicyMLP(layer_sizes=[128, 128])
+    dummy_x0 = jnp.zeros((2,))
+    dummy_U = jnp.zeros((19, 2))
+    dummy_k = jnp.array([10])
+    rng, params_rng = jax.random.split(rng)
+    params = net.init(params_rng, dummy_x0, dummy_U, dummy_k)
+
+    # Learning parameters
+    iterations = 2000
+    batch_size = 256
+    learning_rate = 1e-3
+
+    # Training loop
+    optimizer = optax.adam(learning_rate)
+    opt_state = optimizer.init(params)
+
+    def loss_fn(params, x0, U, k, s):  # noqa: ANN001, N803 (ingore annotations)
+        """Loss function for score model training."""
+        s_pred = net.apply(params, x0, U, k)
+        return jnp.mean(jnp.square(s_pred - s))
+
+    loss_and_grad = jax.value_and_grad(loss_fn)
+    jit_loss = jax.jit(loss_fn)
+
+    @jax.jit
+    def train_step(params, opt_state, rng):  # noqa: ANN001 (ingore annotations)
+        """Perform a single SGD step."""
+        # Sample a batch
+        idxs = jax.random.randint(rng, (batch_size,), 0, len(train_dataset.x0))
+        x0 = train_dataset.x0[idxs]
+        U = train_dataset.U[idxs]
+        k = train_dataset.k[idxs]
+        s = train_dataset.s[idxs]
+
+        # Compute the loss and gradients
+        loss, grad = loss_and_grad(params, x0, U, k, s)
+
+        # Update the parameters
+        updates, opt_state = optimizer.update(grad, opt_state)
+        params = optax.apply_updates(params, updates)
+
+        return params, opt_state, loss
+
+    for iter in range(iterations):
+        rng, batch_rng = jax.random.split(rng)
+        params, opt_state, loss = train_step(params, opt_state, batch_rng)
+
+        if iter % 500 == 0 or iter == iterations - 1:
+            val_loss = jit_loss(
+                params,
+                val_dataset.x0,
+                val_dataset.U,
+                val_dataset.k,
+                val_dataset.s,
+            )
+            print(f"Step {iter}, loss {loss:.4f}, val loss {val_loss:.4f}")
+
+
 if __name__ == "__main__":
     # solve_with_gradient_descent()
-    plot_dataset()
+    # generate_dataset(save=True)
+    fit_score_model()
